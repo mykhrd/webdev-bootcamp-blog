a66d250429e1efe6a4b56d85f5af466473c2b8a6
mykhrd <mykhrd@gmail.com>
2020-06-29 14:26:19 +0900

    add content

--- app.js
+++ app.js
@@ -15,19 +15,17 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+// GET method route
+app.get('/', function (req, res) {
+    res.render('home', {homeContent: homeStartingContent});
+});
+app.get('/about', function (req, res) {
+    res.render('about', {aboutContent: aboutContent});
+});
+app.get('/contact', function (req, res) {
+    res.render('contact', {contactContent: contactContent});
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(3000, function () {
+    console.log("Server started on port 3000");
 });
